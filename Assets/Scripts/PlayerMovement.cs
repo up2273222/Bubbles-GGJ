db9a846055533a255d74dc1e640c678b342b9a73
up2273222 <up2273222@myport.ac.uk>
2025-01-20 12:41:21 +0000

    Add velocity clamp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,34 +16,48 @@ public class PlayerMovement : MonoBehaviour
     //Movement values
     [Header("Movement")]
     public float moveSpeed;
+    public float maxSpeed;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        //Assign components
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get movement direction
         getMoveDir();
     }
 
     void FixedUpdate()
     {
+        //Clamp Velocity
+        capMoveSpeed();
+        //Move player toward mouse cursor if mouse1 is down
         if (Input.GetMouseButton(0))
         {
-            rb.AddForce(moveDir.normalized * moveSpeed, ForceMode2D.Force);
+           rb.AddForce(moveDir.normalized * moveSpeed, ForceMode2D.Force);
         }
+        
     }
 
+    
     private void getMoveDir()
     {
+        //Calculate move direction
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         moveDir = mousePosition - rb.position;
     }
-    
+
+    private void capMoveSpeed()
+    {
+        //Clamp player velocity
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+    }
     
     
 }
