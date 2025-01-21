3ba9ae566e6cc79bd1fdab90c19952af9cf99ea4
up2273222 <up2273222@myport.ac.uk>
2025-01-21 17:31:46 +0000

    Implement menu placeholders

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,27 +9,64 @@ public class MenuController : MonoBehaviour
     public CinemachineVirtualCamera AboveWaterCam;
     public CinemachineVirtualCamera BelowWaterCam;
 
+    public GameObject MenuScreen1;
+    public GameObject MenuScreen2;
+
 
     private void Start()
     {
         AboveWaterCam.Priority = 11;
         BelowWaterCam.Priority = 10;
     }
+    
 
-    private void Update()
+    public void LevelSelect()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (AboveWaterCam.Priority == 11)
-            {
-                AboveWaterCam.Priority = 10;
-                BelowWaterCam.Priority = 11;
-            }
-            else
-            {
-                AboveWaterCam.Priority = 11;
-                BelowWaterCam.Priority = 10;
-            }
-        }
+        AboveWaterCam.Priority = 10;
+        BelowWaterCam.Priority = 11;
+        MenuScreen1.SetActive(false);
+        StartCoroutine(LevelSelectDelay());
+    }
+
+    public void BackButton()
+    {
+        AboveWaterCam.Priority = 11;
+        BelowWaterCam.Priority = 10;
+        MenuScreen2.SetActive(false);
+        StartCoroutine(BackButtonDelay());
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel1()
+    {
+        print("Loading Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        print("Loading Level2");
+    }
+
+    public void LoadLevel3()
+    {
+        print("Loading Level3");
+    }
+
+    private IEnumerator LevelSelectDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        MenuScreen2.SetActive(true);
+    }
+
+    private IEnumerator BackButtonDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        MenuScreen1.SetActive(true);
     }
 }
+
+
