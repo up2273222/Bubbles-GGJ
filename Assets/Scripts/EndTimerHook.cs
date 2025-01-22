1a79d29c32f3a6c189837feb40946551e3429344
up2273222 <up2273222@myport.ac.uk>
2025-01-22 16:05:24 +0000

    Import some assets

--- Assets/Scripts/EndTimerHook.cs
+++ Assets/Scripts/EndTimerHook.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class EndTimerHook : MonoBehaviour
 {
     public float hookMoveSpeed;
+    private bool isPaused = false;
 
     void Start()
     {
@@ -14,15 +15,18 @@ public class EndTimerHook : MonoBehaviour
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y - hookMoveSpeed, transform.position.z);
+        if (!isPaused)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - hookMoveSpeed, transform.position.z);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == 3)
         {
-            other.gameObject.transform.SetParent(gameObject.transform);
-            hookMoveSpeed *= -1;
+            StartCoroutine(ReelBack());
         }
     }
 
@@ -30,7 +34,20 @@ public class EndTimerHook : MonoBehaviour
     private IEnumerator RestartLevel()
     {
         yield return new WaitForSeconds(2f);
+        GlobalManager.Instance.deathCounter++;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
+    }
+
+    private IEnumerator ReelBack()
+    {
+        
+        PlayerController.instance.gameObject.transform.SetParent(gameObject.transform);
+        isPaused = true;
+        yield return new WaitForSeconds(0.5f);
+        isPaused = false;
+        hookMoveSpeed *= -2;
+        StartCoroutine(RestartLevel());
+          
     }
 }
